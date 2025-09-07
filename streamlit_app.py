411cc0b3569a4641857c85eb2cb773d444229fe2
Shraddha Dinesh Aher <shaher@syr.edu>
2025-09-07 00:02:37 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-from HWs.hw1 import run_hw1
-from HWs.hw2 import run_hw2
+from hws.hw1 import run_hw1
+from hws.hw2 import run_hw2
 
 st.set_page_config(page_title="HW Manager")
-st.title("📚 HW Manager")
-st.write("Select a page from the sidebar to continue.")
 
 pages = {
     "HW1 - Document Q&A": run_hw1,
@@ -13,5 +11,4 @@ pages = {
 
 choice = st.sidebar.selectbox("Choose a page", list(pages.keys()))
 
-# Run selected page
 pages[choice]()
